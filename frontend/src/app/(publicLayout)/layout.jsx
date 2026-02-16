2f95de9b5c103fcfff914f4b47baf2a5b3594767
ashadulislam6156rs <ashadulislam6156rs@gamil.com>
2026-02-16 19:27:51 +0600

    [Added]: create new brance and install shadcn/ui

--- frontend/src/app/(publicLayout)/layout.jsx
+++ frontend/src/app/(publicLayout)/layout.jsx
@@ -1,9 +1,13 @@
+import Footer from '@/components/Share/Footer';
+import Navbar from '@/components/Share/Navbar';
 import React from 'react';
 
 const layout = ({children}) => {
     return (
         <div>
+            <Navbar></Navbar>
             {children}
+            <Footer></Footer>
         </div>
     );
 };
